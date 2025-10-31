cf195e0868492d97b5a962c8fc2e0e48f642619d
Gvinu <Gvinu@Vinusha_Mahesh>
2025-10-31 13:48:51 +0530

    testcase for promotion module

--- EPHARMA/src/test/java/com/epharma/testscripts/PromotionsModule.java
+++ EPHARMA/src/test/java/com/epharma/testscripts/PromotionsModule.java
@@ -1,5 +1,12 @@
 package com.epharma.testscripts;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class PromotionsModule {
+	
+	@Test
+	public void doPromotions() {
+		Reporter.log("Promotion given",true);	}
 
 }
